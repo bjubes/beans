2d9281cf255c1d5656dd0b4de3f52b16efb0a958
bjubes <brian2386@gmail.com>
2017-02-03 18:28:50 -0500

    update readme

--- content.js
+++ content.js
@@ -1,11 +1,17 @@
 //check if activated
-var toggleUrl = 
+console.log("BEANS ARE GO");
+
+var go = false;
+var toggleUrl = "https://raw.githubusercontent.com/bjubes/beans/master/status.php"
 $.ajax({
         url: toggleUrl,
         type: "GET",
   		processData: false,
 	    success: function(data) {
-	    	console.log(data);
+	    	if (data == "1") {
+	    		console.log("activating");
+	    		go = true;
+	    	}
 		}
 	});
 
@@ -20,5 +26,8 @@ window.setInterval(function(){
 
 
 function beans() {
-	$("img").attr("src", "https://github.com/bjubes/beans/blob/master/bean.png");
+	if (!go) {
+		return;
+	}
+	$("img").attr("src", "https://raw.githubusercontent.com/bjubes/beans/master/bean.png");
 }
