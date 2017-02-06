fdbb0ddb55a6c9b22c43e088a85a160514a83acf
Brian Jubelirer <brian2386@gmail.com>
2017-02-06 09:22:54 -0500

    recheck for updates every five seconds

--- content.js
+++ content.js
@@ -3,26 +3,32 @@ console.log("BEANS ARE GO");
 
 var go = false;
 var toggleUrl = "https://beansrbeans.herokuapp.com/states/1.json"
-$.ajax({
-        url: toggleUrl,
-        type: "GET",
-        headers: {
-     		'Cache-Control': 'no-cache, no-store, must-revalidate', 
-     		'Pragma': 'no-cache', 
-     		'Expires': '0'
-   		},
-  		processData: false,
-	    success: function(data) {
-	    	response = JSON.parse(data)
-	    	console.log(response);
-	    	if (response["status"]) {
-	    		console.log("activating");
-	    		go = true;
-	    	} else {
-	    	}
-		}
-	});
-
+sendRequest();
+function sendRequest() {
+	$.ajax({
+		url: toggleUrl,
+		type: "GET",
+		headers: {
+			'Cache-Control': 'no-cache, no-store, must-revalidate', 
+			'Pragma': 'no-cache', 
+			'Expires': '0'
+			},
+			processData: false,
+		    	success: function(data) {
+				response = JSON.parse(data)
+				console.log(response);
+				if (response["status"]) {
+					console.log("activating");
+					go = true;
+				}
+				else {
+					if (response["status"] == false && go) {
+						location.reload();
+					}
+				}
+			}
+		});
+}
 
 $( document ).ready(function() {
 	beans();
@@ -32,6 +38,9 @@ window.setInterval(function(){
 	beans();
 }, 1000);
 
+window.setInterval(function(){
+	sendRequest();
+}, 5000);
 
 function beans() {
 	if (!go) {
